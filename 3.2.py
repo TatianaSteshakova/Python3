620a23f71ad228a41b77706fb35d52f7cd8536a8
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-08-11 09:44:54 +0300

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,8 @@
+a1 = input("Введите 1-ую стадию развития человека: ") # Australopithecus
+a2 = input("Введите 2-ую стадию развития человека: ") # Homo habilis
+a3 = input("Введите 3-ую стадию развития человека: ") # Homo erectus
+a4 = input("Введите 4-ую стадию развития человека: ") # Homo neanderthalensis
+a5 = input("Введите 5-ую стадию развития человека: ") # Homo sapiens
+a6 = input("Введите 6-ую стадию развития человека: ") # Homo sapiens sapiens
+
+print (a1, a2, a3, a4, a5, a6, sep="=>")
